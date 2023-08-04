91959c87ecc4ddf2a86a6defe0616c3f8a83992d
graciasvito <vitoristo1@gmail.com>
2023-08-04 16:08:15 +0700

    fix dev dependencies

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -23,19 +23,21 @@ export default function Detail() {
   };
   return (
     <>
-      <Header />
-      <div className="header-container detail-container">
-        <div className="detail-hero">
-          <img src={data[0]?.image} alt="Image Product" />
-          <div className="detail-desc">
-            <h1>{data[0]?.name}</h1>
-            <p className="price">Rp{data[0]?.harga}</p>
-            <label>Description</label>
-            <p>{data[0]?.description}</p>
+      <div className="detail-header">
+        <Header />
+        <div className="header-container detail-container">
+          <div className="detail-hero">
+            <img src={data[0]?.image} alt="Image Product" />
+            <div className="detail-desc">
+              <h1>{data[0]?.name}</h1>
+              <p className="price">Rp{data[0]?.harga}</p>
+              <label>Description</label>
+              <p>{data[0]?.description}</p>
+            </div>
           </div>
         </div>
+        <Header />
       </div>
-      <Header />
     </>
   );
 }
